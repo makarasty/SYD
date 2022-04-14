2124499ab3c914ac6c24e214a5ab38980fcdadc5
makarasty <71918286+makarasty@users.noreply.github.com>
2022-04-14 17:44:52 +0400

    Delete BETA_MaketStatus/data directory

--- BETA_MaketStatus/data/discord.js/src/util/SystemChannelFlags.js
+++
@@ -1,8 +0,0 @@
-const BitField = require('./BitField');
-class SystemChannelFlags extends BitField {}
-SystemChannelFlags.FLAGS = {
-    WELCOME_MESSAGE_DISABLED: 1 << 0,
-    BOOST_MESSAGE_DISABLED: 1 << 1,
-};
-
-module.exports = SystemChannelFlags;
